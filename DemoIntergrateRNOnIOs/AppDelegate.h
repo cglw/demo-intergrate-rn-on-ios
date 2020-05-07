ca69fd114b0b97177b4e75433a367990223b3856
namlh4 <namlh4@vng.com.vn>
2020-05-07 18:32:15 +0700

    integrate react-native bridge to existing demo-app

--- DemoIntergrateRNOnIOs/AppDelegate.h
+++ DemoIntergrateRNOnIOs/AppDelegate.h
@@ -5,11 +5,12 @@
 //  Created by Nam Le Hoang on 4/28/20.
 //  Copyright © 2020 Nam Le Hoang. All rights reserved.
 //
-
+#import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
+@property (nonatomic, strong) UIWindow *window;
 
 @end
 
