5619bed356b52873f36b462833333d11682d2854
namlh4 <namlh4@vng.com.vn>
2020-05-07 18:33:28 +0700

    add native button to navigate to react-native module

--- DemoIntergrateRNOnIOs/ViewController.m
+++ DemoIntergrateRNOnIOs/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -18,6 +19,34 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)handleLaunchReactNativeAppPressed:(id)sender {
+    NSLog(@"User pressed react-native button");
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+     RCTRootView *rootView =
+         [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+                                     moduleName: @"Hero"
+                         initialProperties:
+                           @{
+                             @"scores" : @[
+                               @{
+                                 @"name" : @"Superman",
+                                 @"value": @"100"
+                                },
+                               @{
+                                 @"name" : @"Batman",
+                                 @"value": @"20"
+                               },
+                               @{
+                                 @"name" : @"The Flash",
+                                 @"value": @"60"
+                               }
+                             ]
+                           }
+                             launchOptions: nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
 
-
+ 
 @end
