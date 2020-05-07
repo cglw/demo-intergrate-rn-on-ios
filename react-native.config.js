0bcbba2f314cde72a1d030d68dbb4ccdb32abbc0
namlh4 <namlh4@vng.com.vn>
2020-05-07 09:50:21 +0700

    handle install library from local node modules

---
+++ react-native.config.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+'use strict';
+
+const ios = require('./node_modules/@react-native-community/cli-platform-ios');
+const android = require('./node_modules/@react-native-community/cli-platform-android');
+
+module.exports = {
+  commands: [...ios.commands, ...android.commands],
+  platforms: {
+    ios: {
+      linkConfig: ios.linkConfig,
+      projectConfig: ios.projectConfig,
+      dependencyConfig: ios.dependencyConfig,
+    },
+    android: {
+      linkConfig: android.linkConfig,
+      projectConfig: android.projectConfig,
+      dependencyConfig: android.dependencyConfig,
+    },
+  }
+};
